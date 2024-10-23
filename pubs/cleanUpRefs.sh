1173486c8a9706d4e8585091575379356d6d5c00
georgebusby <george.busby@gmail.com>
2024-10-23 11:03:14 +0100

    updated pubs with orcid/google scholar

--- pubs/cleanUpRefs.sh
+++ pubs/cleanUpRefs.sh
@@ -4,9 +4,11 @@
 
 sed 's#\\coloremphasis##g' my_refs.html > tmp
 
+# this part doesn't acgtually work...
 for i in {1..33}; do
-   sed 's#<div class="csl-left-margin">['$i']</div><div class="csl-right-inline">#<div class="csl-right-inline">['$i']#g' tmp > tmp1
-   mv tmp1 tmp
+    print $i
+    sed 's#<div class="csl-left-margin">['$i']</div><div class="csl-right-inline">#<div class="csl-right-inline">['$i']#g' tmp > tmp1
+    mv tmp1 tmp
 done
 
 mv tmp my_refs.html
